205ee4b36fb6e257699f3db7df68b8707a1d702f
Yogurt <haoanh2003@gmail.com>
2024-09-25 18:06:54 +0700

    layout admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',function(){
-    return view('home.index.home');
+// Admin
+Route::get('/', function() {
+    return view('admin.index.home');
 });
+
+// 
+
