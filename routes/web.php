ba2c6cf8c41fae58e903650fd6c9ae8ab3b5bac8
Thiên Đức <lebuithienduc123@gmail.com>
2024-09-26 00:07:26 +0700

    Update admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Bill\BillController;
+use App\Http\Controllers\ShoppingCart\Cart;
+use App\Http\Controllers\ShoppingCart\CartController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',function(){
     return view('home.index.home');
-});
+})->name('home');
+Route::get('/Cart',[CartController::class,'show'])->name("ShowCart");
+Route::get('/Checkout',[BillController::class,'Checkout'])->name("Checkout");
+Route::get('/Checkout/success',[BillController::class,'Success'])->name("Success");
